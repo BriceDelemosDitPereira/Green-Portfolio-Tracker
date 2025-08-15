673ba4c2130768d2a84909bbd4f510b3b23dec83
bdelemos <delemos@hotmail.fr>
2025-08-15 19:30:41 +0200

    Added : Second test passed

--- green_portfolio_tracker/api/tests.py
+++ green_portfolio_tracker/api/tests.py
@@ -4,6 +4,9 @@ from rest_framework.test import APIClient # APIClient is used to test API endpoi
 from green_portfolio_tracker.api.models import Investment, PortfolioTransaction
 from django.contrib.auth.models import User
 
+# Check this tests with : pytest -v green_portfolio_tracker/api/tests.py
+# -v for verbose output
+
 @pytest.fixture # Fixture is used to create reusable test data
 def client():
     return APIClient() # Fixture to provide an API client for tests
@@ -28,3 +31,14 @@ def test_create_investment(client, user):
     assert response.status_code == 201 # assert is used to check if the condition is true
     assert Investment.objects.count() == 1
     assert Investment.objects.first().name == 'Wind Farm'
+
+# Test2 for Transction_summary
+@pytest.mark.django_db # Need to precise this again to use the Django database
+def test_transaction_summary(client, user):
+    investment = Investment.objects.create(name='Solar', co2_reduction_per_unit=2.0, expected_return=4.0)
+    PortfolioTransaction.objects.create(user=user, investment=investment, amount=1000)
+    url = '/api/transactions/summary/'
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.data['total_impact'] == 2000.0 # 1000 * 2.0
+    assert response.data['total_performance'] == 40.0 # 1000 * (4.0 / 100)
--- green_portfolio_tracker/api/views.py
+++ green_portfolio_tracker/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets, permissions # viewsets is used to create views that provide CRUD operations for models, and permissions are used to control access to these views. (CRUD : Create, Read, Update, Delete)
 from .models import Investment, PortfolioTransaction
 from .serializers import InvestmentSerializer, PortfolioTransactionSerializer
-from django.db.models import Sum
+from django.db.models import F, Sum, FloatField
 from rest_framework.response import Response
 from rest_framework.decorators import action
 
@@ -17,15 +17,16 @@ class PortfolioTransactionViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return PortfolioTransaction.objects.filter(user=self.request.user)
     
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
     # @action is used to add custom actions to the viewset
     @action(detail=False, methods=['get'])
     def summary(self, request):
         qs = self.get_queryset()
-        total_impact = qs.aggregate(Sum('impact'))['impact__sum'] or 0
-        total_performance = qs.aggregate(Sum('performance'))['performance__sum'] or 0
+        total_impact = qs.annotate(
+            impact=F('amount') * F('investment__co2_reduction_per_unit')
+        ).aggregate(total_impact=Sum('impact', output_field=FloatField()))['total_impact'] or 0
+        total_performance = qs.annotate(
+            performance=F('amount') * F('investment__expected_return') / 100
+        ).aggregate(total_performance=Sum('performance', output_field=FloatField()))['total_performance'] or 0
         return Response({
             'total_impact': total_impact,
             'total_performance': total_performance
