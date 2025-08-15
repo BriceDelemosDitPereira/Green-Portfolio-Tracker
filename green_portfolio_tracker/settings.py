0a5bdddeec9997f16342cf87c88a0a777d136c6f
bdelemos <delemos@hotmail.fr>
2025-08-15 16:19:00 +0200

    Configured settings.py with API

--- green_portfolio_tracker/settings.py
+++ green_portfolio_tracker/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', # Django REST framework for API support
+    'api', # Custom app for API endpoints
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
