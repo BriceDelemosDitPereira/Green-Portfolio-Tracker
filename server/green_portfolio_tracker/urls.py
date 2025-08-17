67f28eb8be829570b66a2bc71e391d390bcab773
bdelemos <delemos@hotmail.fr>
2025-08-17 17:13:02 +0200

    Replaced backend index.html with redirect to /api/

--- server/green_portfolio_tracker/urls.py
+++ server/green_portfolio_tracker/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 from rest_framework.routers import DefaultRouter
 from green_portfolio_tracker.api.views import InvestmentViewSet, PortfolioTransactionViewSet
 
@@ -9,7 +10,11 @@ router = DefaultRouter()
 router.register(r'investments', InvestmentViewSet)
 router.register(r'transactions', PortfolioTransactionViewSet, basename='transactions')
 
+def redirect_to_api(request):
+    return HttpResponseRedirect('/api/')
+
 urlpatterns = [
+    path('', redirect_to_api, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
